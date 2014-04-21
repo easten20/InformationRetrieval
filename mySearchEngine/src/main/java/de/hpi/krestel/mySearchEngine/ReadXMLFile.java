81b6faab50fff9564fdfecb4ba1f9e08c86960d9
easten20 <easten_20@hotmail.com>
2014-04-21 14:41:20 +0200

    rename page object to Wikipage put class Wikipage into seperate file (WikiPage.java) refactor ReadXMLFile.java

--- mySearchEngine/src/main/java/de/hpi/krestel/mySearchEngine/ReadXMLFile.java
+++ mySearchEngine/src/main/java/de/hpi/krestel/mySearchEngine/ReadXMLFile.java
@@ -1,98 +1,96 @@
 package de.hpi.krestel.mySearchEngine;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
- 
-class Page {   
-    String title;
-    String id;   
-    public Page(){        
-    }
-}
 
 
-public class ReadXMLFile {
- 
-   public static void main(String argv[]) {
- 
-    try {
- 
-	SAXParserFactory factory = SAXParserFactory.newInstance();
-	SAXParser saxParser = factory.newSAXParser();	
- 
-	DefaultHandler handler = new DefaultHandler() {
-	
-		
-	List<Page> pageL = new ArrayList<Page>();
-	Page page;
-	String tmpValue;
-	boolean btitle = false;
-	boolean bid = false;	
-	boolean isPage = false;
-    int limit = 5;
-    
-	public void startElement(String uri, String localName,String elementName, 
-                Attributes attributes) throws SAXException {
- 
-		
-		//System.out.println("Start Element :" + elementName);
- 
-		if (elementName.equalsIgnoreCase("page")) {			
-			 page = new Page();			
-			 isPage = true;			 			 			
+/**
+ * @author easten
+ * XML parser class to parse xml file
+ */
+public class ReadXMLFile extends DefaultHandler {
+				
+		public List<WikiPage> pageL = new ArrayList<WikiPage>();
+		WikiPage page;
+		String tmpValue;		
+		String fileName;
+		boolean isPage = false;
+	    int limit = 5;
+	    
+	    public ReadXMLFile(String fileName) {
+	    	this.fileName = fileName;
+	    	parseDocument();
+	    }
+	    
+		public void startElement(String uri, String localName,String elementName, 
+	                Attributes attributes) throws SAXException {	
+			
+			//System.out.println("Start Element :" + elementName);
+	 
+			if (elementName.equalsIgnoreCase("page")) {			
+				 page = new WikiPage();			
+				 isPage = true;			 			 			
+			}
 		}
-	}
- 
-	public void endElement(String uri, String localName,
-		String element) throws SAXException {
-		 if (isPage) {
-		        if (element.equals("id") && !(page.id != null)) 		        	
-		            page.id = this.tmpValue;
-		        else if (element.equals("title"))
-		        	page.title = this.tmpValue;
-		        else if (element.equals("page"))
-		        {
-		        	pageL.add(page);
-		        	isPage = false;
-		        	limit = limit - 1;		        	
-		        	if (limit == 0)
-		        	{		        		 
-		        		for (Page pages : pageL) {		        			 
-		        			System.out.println(pages.id);
-		        			System.out.println(pages.title);
-		        		}		        			 		        			     
-		        		throw new SAXException(new Exception());
-		        	}
-		        }
-		    }
-		//System.out.println("End Element :" + element);
- 
-	}
- 
-	public void characters(char ch[], int start, int length) throws SAXException {		
-			tmpValue = new String(ch, start, length);					
-	}
- 
-     };
- 
-     try{
-       saxParser.parse("C:\\Users\\easten\\Documents\\Information Retrieval\\dewiki-20140216-pages-articles-multistream.xml", handler);
-     }
-       catch (SAXException e) {
-    	    if (e instanceof Exception) {
-    	    	System.out.println("Finish Parsing"); 	        
-    	    }
-    	}
-     } catch (Exception e) {
-       e.printStackTrace();
-     }
+		
+		 private void parseDocument() {			 
+			         SAXParserFactory factory = SAXParserFactory.newInstance();			 
+			         try {			 
+			             SAXParser parser = factory.newSAXParser();			 
+			             parser.parse(this.fileName, this);			 
+			         } catch (ParserConfigurationException e) {			
+			             System.out.println("ParserConfig error");			
+			         } catch (SAXException e) {		
+			             System.out.println("finish parsing xml");			
+			         } catch (IOException e) {		
+			             System.out.println("IO error");			
+			         }			 
+			     }
+
+	 
+		public void endElement(String uri, String localName,
+			String element) throws SAXException {
+			 if (isPage) {
+			        if (element.equals("id") && !(page.id != null)) 		        	
+			            page.id = this.tmpValue;
+			        else if (element.equals("title"))
+			        	page.title = this.tmpValue;
+			        else if (element.equals("page"))
+			        {
+			        	pageL.add(page);
+			        	isPage = false;
+			        	--limit;		        	
+			        	// throws error when the parser reach its limit
+			        	if (limit == 0)			        	        		 			        		    			   
+			        		throw new SAXException();			        	
+			        }
+			    }
+			//System.out.println("End Element :" + element);
+	 
+		}
+	 
+		public void characters(char ch[], int start, int length) throws SAXException {		
+				tmpValue = new String(ch, start, length);					
+		}
+	 	
+	
+   public static void main(String argv[]) {
+     
+ 	
+	ReadXMLFile handler = new ReadXMLFile("C:\\Users\\easten\\Documents\\Information Retrieval\\dewiki-20140216-pages-articles-multistream.xml");
+    
+     for (WikiPage page : handler.pageL)
+  	   System.out.println("title :" + page.title);     
  
    }
- 
+   
 }
+ 
---
+++ mySearchEngine/src/main/java/de/hpi/krestel/mySearchEngine/WikiPage.java
@@ -0,0 +1,15 @@
+package de.hpi.krestel.mySearchEngine;
+
+
+/**
+ * @author easten
+ * class to contain pages from xml file
+ */
+public class WikiPage {
+	  String title;
+	    String id;   
+	    String text;
+	    
+	    public WikiPage(){        
+	    }
+}
