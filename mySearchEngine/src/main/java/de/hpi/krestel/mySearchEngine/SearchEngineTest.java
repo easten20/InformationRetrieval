b65a979fa785b4a77d27c3a89849a42ae77acba7
Jaeyoon Jung <jaeyoon.jung@student.hpi.uni-potsdam.de>
2014-05-01 12:05:34 +0200

    “basicpath”

--- mySearchEngine/src/main/java/de/hpi/krestel/mySearchEngine/SearchEngineTest.java
+++ mySearchEngine/src/main/java/de/hpi/krestel/mySearchEngine/SearchEngineTest.java
@@ -25,12 +25,12 @@ public class SearchEngineTest {
 
 	public static void main(String[] args) throws IOException, XMLStreamException {
 		String basicPath = "res/dewiki-20140216-pages-articles-multistream.xml";
-		//basicPath = "res/wiki.xml"; // comment this out/in
+		basicPath = "res/wiki.xml"; // comment this out/in
 		//basicPath = "res/dewiki-20140216-pages-articles-multistream.10.xml";
 		//basicPath = "res/dewiki-20140216-pages-articles-multistream.100.xml";
 		//basicPath = "res/dewiki-20140216-pages-articles-multistream.1000.xml";
-		basicPath = "res/dewiki-20140216-pages-articles-multistream.10000.xml";
-		basicPath = "res/dewiki-20140216-pages-articles-multistream.100000.xml";
+		//basicPath = "res/dewiki-20140216-pages-articles-multistream.10000.xml";
+		//basicPath = "res/dewiki-20140216-pages-articles-multistream.100000.xml";
 		
 		String filePath = new File(basicPath).getAbsolutePath();
 		SearchEngineY test = new SearchEngineY();
